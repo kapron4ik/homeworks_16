33316effedc3556481636613117c63546eb0150d
Vitaliy <kapron4ik@mail.ru>
2021-07-09 05:14:38 +0300

    API

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,8 +5,8 @@ import {AppStateType} from "../redux/store";
 import {CardReqType, CardsReqType, NewPackType, PacksReqType, PacksType} from "../types/entities";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:7542/2.0/',
-    // baseURL: 'https://neko-back.herokuapp.com/2.0',
+    // baseURL: 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0',
     withCredentials: true
 })
 
